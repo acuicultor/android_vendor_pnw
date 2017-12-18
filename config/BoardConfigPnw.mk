31936d2e00d34ef2dd6bc33339c73426ec1fc3e7
acuicultor <ramoncuenca4@gmail.com>
2017-12-18 11:54:02 +0100

    Nuke offline charger and healthd for now

--- config/BoardConfigPnw.mk
+++ config/BoardConfigPnw.mk
@@ -1,4 +1,4 @@
 # Charger
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-    BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
-endif
+# ifneq ($(WITH_LINEAGE_CHARGER),false)
+#     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
+# endif
